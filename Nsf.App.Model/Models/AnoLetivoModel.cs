39f08e88f7ecba48029a4ed07694c94249eb916c
infob02 <infob02@INFO07>
2019-11-29 10:17:15 -0200

    Caio: Model AnoLetivo criado

---
+++ Nsf.App.Model/Models/AnoLetivoModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nsf.App.Model.Models
+{
+    public class AnoLetivoModel
+    {
+        public int IdAnoLetivo{ get; set; }
+
+        public int NrAno { get; set; }
+
+        public DateTime DtInicio { get; set; }
+
+        public DateTime DtFim { get; set; }
+
+        public bool BtAtivo { get; set; }
+
+        public string TpStatus { get; set; }
+    }
+}
