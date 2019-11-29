92e5a1f6cfcc06fe24d0cd4f69f643245fae9c4f
Luan <teste@INFO14>
2019-11-29 10:34:09 -0200

    LUAN

--- Nsf.App.API.Client/APIs/ProfessorApi.cs
+++ Nsf.App.API.Client/APIs/ProfessorApi.cs
@@ -20,36 +20,36 @@ namespace Nsf.App.API.Client.APIs
             var resp = client.PostAsync("http://localhost:5000/professor/", body).Result;
         }
 
-        //public void Alterar(Model.Entities.tb_professor professor)
-        //{
-        //    string json = JsonConvert.SerializeObject(professor);
-        //    StringContent body = new StringContent(json, Encoding.UTF8, "application/json");
+        public void Alterar(Nsf.App.Model.ProfessorModel professor)
+        {
+            string json = JsonConvert.SerializeObject(professor);
+            StringContent body = new StringContent(json, Encoding.UTF8, "application/json");
 
-        //    var resp = client.PutAsync("http://localhost:5000/professor/", body).Result;
-        //}
+            var resp = client.PutAsync("http://localhost:5000/professor/", body).Result;
+        }
 
-        //public void Deletar(int id)
-        //{
-        //    var resp = client.DeleteAsync("http://localhost:5000/professor/" + id).Result;
-        //}
+        public void Deletar(int id)
+        {
+            var resp = client.DeleteAsync("http://localhost:5000/professor/" + id).Result;
+        }
 
-        //public List<Model.Entities.tb_professor> ListarTodos()
-        //{
-        //    string json = client.GetAsync("http://localhost:5000/professor/").Result.Content.ReadAsStringAsync().Result;
+        public List<Nsf.App.Model.ProfessorModel> ListarTodos()
+        {
+            string json = client.GetAsync("http://localhost:5000/professor/").Result.Content.ReadAsStringAsync().Result;
 
-        //    List<Model.Entities.tb_professor> list = JsonConvert.DeserializeObject<List<Model.Entities.tb_professor>>(json);
+            List<Nsf.App.Model.ProfessorModel> list = JsonConvert.DeserializeObject<List<Nsf.App.Model.ProfessorModel>>(json);
 
-        //    return list;
-        //}
+            return list;
+        }
 
-        //public List<Model.Entities.tb_professor professor> ConsultarPorNome(string nome)
-        //{
-        //    string json = client.GetAsync("http://localhost:5000/professor/nome/" + nome).Result.Content.ReadAsStringAsync().Result;
+        public List<Nsf.App.Model.ProfessorModel> ConsultarPorNome(string nome)
+        {
+            string json = client.GetAsync("http://localhost:5000/professor/nome/" + nome).Result.Content.ReadAsStringAsync().Result;
 
-        //    List<Model.Entities.tb_professor> list = JsonConvert.DeserializeObject<List<Model.Entities.tb_professor>>(json);
+            List<Nsf.App.Model.ProfessorModel> list = JsonConvert.DeserializeObject<List<Nsf.App.Model.ProfessorModel>>(json);
 
-        //    return list;
-        //}
+            return list;
+        }
 
 
 
