d008c0b9e78f70ea30258c9f47af28da0d230c73
teste <teste@INFO03>
2019-11-29 10:05:02 -0300

    ;;

--- Nsf.App.API.Client/CursoApi.cs
+++ Nsf.App.API.Client/CursoApi.cs
@@ -1,13 +1,27 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Nsf.App.API.Client
 {
-    class CursoApi
+    public class CursoApi
     {
-        public List<Model.>
+        public List<Model.CursoModel> ListarTodos()
+        {
+            HttpClient client = new HttpClient();
+
+            string json = client.GetAsync("http//:localhost:5000/Curso/")
+                                .Result
+                                .Content
+                                .ReadAsStringAsync()
+                                .Result;
+
+            List<Model.CursoModel> cursos = JsonConvert.DeserializeObject<List<Model.CursoModel>>(json);
+            return cursos;
+        }
     }
 }
