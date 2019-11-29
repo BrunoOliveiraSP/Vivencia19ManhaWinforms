2438ad554e1c74dc443f040dcfe4b313e1075310
Matheus Lopes Gimenez <gimenez.lopes@hotmail.com>
2019-11-29 16:44:50 -0300

    Consultar funfa by TITI

--- Nsf.App.API.Client/DisciplinaAPI.cs
+++ Nsf.App.API.Client/DisciplinaAPI.cs
@@ -51,6 +51,7 @@ namespace Nsf.App.API.Client
         public List<DiciplinaModel> ListarDisciplina(string disciplina)
         {
             string json;
+
             if (string.IsNullOrEmpty(disciplina))
             {
                 json = client.GetAsync("http://localhost:5000/Diciplina/ConsultarTudo")
