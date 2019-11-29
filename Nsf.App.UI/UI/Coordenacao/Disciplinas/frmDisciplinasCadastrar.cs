a7c34628b9ffa0fe67f934b6e940e1dad5ec0ec2
Bruno Evaristo <bruno_evaristo23@hotmail.com>
2019-11-29 19:00:05 -0200

    BrunoVinicius:cadastrar diciplina inseri massem o datadeinclusao

--- Nsf.App.UI/UI/Coordenacao/Disciplinas/frmDisciplinasCadastrar.cs
+++ Nsf.App.UI/UI/Coordenacao/Disciplinas/frmDisciplinasCadastrar.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Windows.Forms;
+using Nsf.App.Model.Model;
 
 namespace Nsf.App.UI
 {
@@ -12,6 +14,23 @@ namespace Nsf.App.UI
 
         private void btnSalvar_Click(object sender, EventArgs e)
         {
+            try
+            {
+                DiciplinaModel model = new DiciplinaModel();
+                model.NmDisciplina = txtDisciplina.Text;
+                model.DsSigla = txtSigla.Text;
+                model.BtAtivo = Convert.ToUInt64(chkAtivo.Checked);
+                //falta so colocar data de inclusao
+
+                API.Client.DisciplinaAPI api = new API.Client.DisciplinaAPI();
+                api.Inserir(model);
+
+                MessageBox.Show("Diciplina inserida");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro,Tente Novamante");
+            }
         }
     }
 
