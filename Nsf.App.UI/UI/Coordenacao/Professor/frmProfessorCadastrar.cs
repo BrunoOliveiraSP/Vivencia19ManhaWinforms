f19a865fff6f7a421b4aec2650e8585bd890ca46
Luan <teste@INFO14>
2019-11-29 12:17:23 -0200

    professor

--- Nsf.App.UI/UI/Coordenacao/Professor/frmProfessorCadastrar.cs
+++ Nsf.App.UI/UI/Coordenacao/Professor/frmProfessorCadastrar.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Windows.Forms;
 
 namespace Nsf.App.UI
 {
@@ -14,30 +15,35 @@ namespace Nsf.App.UI
 
         private void btnSalvar_Click(object sender, EventArgs e)
         {
-            //Nsf.App.Model.ProfessorModel prof = new Model.ProfessorModel();
+            Nsf.App.Model.ProfessorModel prof = new Model.ProfessorModel();
 
-            //prof.NmProfessor = txtNome.Text;
-            //prof.NmPai = txtPai.Text;
-            //prof.NmMae = txtNome.Text;
-            //prof.BtAtivo = txtPai.Text;
-            //prof.DsCelular = txtNome.Text;
-            //prof.DsCpf = txtPai.Text;
-            //prof.DsCurso = txtNome.Text;
-            //prof.DsCvLattes = txtPai.Text;
-            //prof.DsEmail = txtNome.Text;
-            //prof.DsEstado = txtPai.Text;
-            //prof.DsFaculdade = txtNome.Text;
-            //prof.DsRg = txtPai.Text;
-            //prof.DsRgEmissor = txtNome.Text;
-            //prof.DsRgOrgao = txtPai.Text;
-            //prof.DsTelefone = txtNome.Text;
-            //prof.DtFaculdadeFim = txtPai.Text;
-            //prof.DtFaculdadeInicio = txtNome.Text;
-            //prof.DtNascimento = txtPai.Text;
-            //prof.IdLogin = txtNome.Text;
-            //prof.NrAnoPrimeiroEmprego = txtPai.Text;
-            //prof.TpContratacao = txtNome.Text;
+            prof.NmProfessor = txtNome.Text;
+            prof.NmPai = txtPai.Text;
+            prof.NmMae = txtNome.Text;
+            prof.BtAtivo = chkAtivo.Checked;
+            prof.DsCelular = txtCelular.Text;
+            prof.DsCpf = txtCpf.Text;
+            prof.DsCurso = txtCurso.Text;
+            prof.DsCvLattes = txtCvLattes.Text;
+            prof.DsEmail = txtEmail.Text;
+            prof.DsEstado = txtEstadoNasc.Text;
+            prof.DsFaculdade = txtFaculdade.Text;
+            prof.DsRg = txtRG.Text;
+            prof.DsRgEmissor = txtRGEmissao.Text;
+            prof.DsRgOrgao = txtRGOrgao.Text;
+            prof.DsTelefone = txtTelefone.Text;
+            prof.DtFaculdadeFim = dtpFaculdadeFim.Value;
+            prof.DtFaculdadeInicio = dtpFaculdadeInicio.Value;
+            prof.DtNascimento = dtpNascimento.Value;
+            prof.IdLogin = Convert.ToInt32(txtLogin.Text);
+            prof.NrAnoPrimeiroEmprego = Convert.ToInt32(nudPrimeiroEmprego.Value);
+            prof.TpContratacao = cboContrato.Text;
 
+
+            Nsf.App.API.Client.ProfessorApi Api = new API.Client.ProfessorApi();
+            Api.Inserir(prof);
+
+            MessageBox.Show("Inserido com sucesso", "NSF");
 
         }
     }
