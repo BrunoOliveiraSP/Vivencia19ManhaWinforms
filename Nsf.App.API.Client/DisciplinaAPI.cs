b9d552022b18ada3669a6cfd244c5b1007c5cebb
Matheus Lopes Gimenez <gimenez.lopes@hotmail.com>
2019-11-30 10:06:15 -0300

    Vá by TITI

--- Nsf.App.API.Client/DisciplinaAPI.cs
+++ Nsf.App.API.Client/DisciplinaAPI.cs
@@ -37,6 +37,7 @@ namespace Nsf.App.API.Client
         {
             try
             {
+
                 string json = JsonConvert.SerializeObject(diciplina);
 
                 StringContent body = new StringContent(json, Encoding.UTF8, "application/json");
