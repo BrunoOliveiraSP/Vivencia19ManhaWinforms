e1482b8a7fa3665724104ff61640bec29c34f01a
freivivencia2019 <freivivencia2019@gmail.com>
2019-12-02 04:08:00 -0300

    aaa

---
+++ Nsf.App.API.Client/SalaVestibularAPI.cs
@@ -0,0 +1,62 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nsf.App.API.Client
+{
+    public class SalaVestibularAPI
+    {
+        HttpClient client = new HttpClient();
+
+
+        public void Inserir(Model.SalaVestibularModel modelo)
+        {
+            string json = JsonConvert.SerializeObject(modelo);
+            StringContent body = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var resp = client.PostAsync("http://localhost:5000/SalaVestibular/", body).Result.Content.ReadAsStringAsync().Result;
+            VerificarErro(resp);
+        }
+        public List<Model.SalaVestibularModel> ListarTodos()
+        {
+            string json = client.GetAsync("http://localhost:5000/SalaVestibular/").Result.Content.ReadAsStringAsync().Result;
+            VerificarErro(json);
+
+            List<Model.SalaVestibularModel> salas = JsonConvert.DeserializeObject<List<Model.SalaVestibularModel>>(json);
+            return salas;
+        }
+        public Model.SalaVestibularModel BuscarPorID(int id)
+        {
+            string json = client.GetAsync("http://localhost:5000/SalaVestibular/" + id).Result.Content.ReadAsStringAsync().Result;
+            VerificarErro(json);
+
+            Model.SalaVestibularModel sala = JsonConvert.DeserializeObject<Model.SalaVestibularModel>(json);
+            return sala;
+        }
+        public void Alterar(Nsf.App.Model.SalaVestibularModel modelo)
+        {
+            string json = JsonConvert.SerializeObject(modelo);
+            StringContent body = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var resp = client.PutAsync("http://localhost:5000/SalaVestibular/", body).Result.Content.ReadAsStringAsync().Result;
+            VerificarErro(resp);
+        }
+        public void Deletar(int id)
+        {
+            var resp = client.DeleteAsync("http://localhost:5000/SalaVestibular/" + id).Result.Content.ReadAsStringAsync().Result;
+            VerificarErro(resp);
+        }
+        private void VerificarErro(string resposta)
+        {
+            if (resposta.Contains("CodigoErro"))
+            {
+                Model.ErroModel erro = JsonConvert.DeserializeObject<Model.ErroModel>(resposta);
+                throw new ArgumentException(erro.Mensagem);
+            }
+        }
+    }
+}
---
+++ Nsf.App.Model/SalaVestibularModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nsf.App.Model
+{
+    public class SalaVestibularModel
+    {
+        public int idSalaVestibular { get; set; }
+        public int idSala { get; set; }
+        public string dsPeriodo { get; set; }
+        public int nrOrdem { get; set; }
+        public int qtInscritos { get; set; }
+        public object idSalaNavigation { get; set; }
+    }
+}
