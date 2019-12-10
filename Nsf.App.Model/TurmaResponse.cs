cc82d70d84c64f4e307d0ca7dce2d888b5061ef9
teste <teste@INFO52>
2019-12-10 12:04:13 -0300

    Caio: Response de Turma funcionando

--- Nsf.App.Model/TurmaResponse.cs
+++ Nsf.App.Model/TurmaResponse.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Nsf.App.API.Client
+namespace Nsf.App.Model
 {
     public class TurmaResponse
     {
@@ -15,7 +15,6 @@ namespace Nsf.App.API.Client
         public string TpPeriodo { get; set; }
         public int NrCapacidadeMax { get; set; }
 
-        public int NrAno { get; set; }
 
         public string NmCurso { get; set; }
     }
